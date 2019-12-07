05f6da3bfed1eef3aa5e19e14ee16015ee84b305
Nick <nicmc9@gmail.com>
2019-12-07 18:46:40 +0400

    sheder rebuild

--- learngl.cpp
+++ learngl.cpp
@@ -16,21 +16,119 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Shader.h"
+#include "shader_m.h"
 
 GLuint VBO, VAO, EBO;
 
 GLuint texture1,texture2;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+auto cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 upWorld = glm::vec3(0.0f, 1.0f, 0.0f);
 
 
+GLfloat deltaTime = 0.0f;	// Время, прошедшее между последним и текущим кадром
+GLfloat lastFrame = 0.0f;  	// Время вывода последнего кадра
+
+GLfloat yaw = -90.0f;
+GLfloat pitch = 0.0f;
+
+bool keys[1024];
+//bool firstMouse = true;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+GLfloat lastX = 400 , lastY = 300;
+GLfloat fov = 45.0f;
+
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+
+
+	
 	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
 	// и приложение после этого закроется
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+
+
+	///Замененно на glfwSetCursorPos
+	//if (firstMouse) // эта переменная была проинициализирована значением true
+	//{
+	//	lastX = xpos;
+	//	lastY = ypos;
+	//	firstMouse = false;
+	//}
+
+	
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos; // Обратный порядок вычитания потому что оконные Y-координаты возрастают с верху вниз 
+	lastX = xpos;
+	lastY = ypos;
+
+	GLfloat sensitivity = 0.05f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	front.y = sin(glm::radians(pitch));
+	front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+	cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
+}
+///Теперь проверки на каждой итерации цикла
+void do_movement()
+{
+	GLfloat cameraSpeed = 5.0f * deltaTime;
+
+	if (keys[GLFW_KEY_W]) {
+		auto temp = cameraSpeed * cameraFront;
+		std::cout << "temp == vec3(" << temp.x << ", " << temp.y << ", " << temp.z << ") " << '\n';
+		std::cout << "cameraPos == vec3(" << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << ") " << '\n';
+		cameraPos += temp;
+		std::cout << "cameraPos == vec3(" << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << ") " << '\n';
+
+		auto pf = cameraPos + cameraFront;
+		std::cout << "pf == vec3(" << pf.x << ", " << pf.y << ", " << pf.z << ") " << '\n';
+	}
+	if (keys[GLFW_KEY_S]) {
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	if (keys[GLFW_KEY_A]) {
+		auto temp = glm::normalize(glm::cross(cameraFront, upWorld)) * cameraSpeed;
+		std::cout << "temp == vec3(" << temp.x << ", " << temp.y << ", " << temp.z << ") " << '\n';
+		std::cout << "cameraPos == vec3(" << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << ") " << '\n';
+		//Без нормализации скорость была бы постоянной
+		cameraPos -= temp;
+	}
+	if (keys[GLFW_KEY_D]) {
+		cameraPos += glm::normalize(glm::cross(cameraFront, upWorld)) * cameraSpeed;
+	}
+
+	
 }
 
 void init() {
@@ -193,8 +291,13 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetCursorPos(window, lastX, lastY);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -238,7 +341,7 @@ int main()
 	///Camera
 
 	///Оказывается важна позиция для LookAt  Ведь это поставщики данныых
-	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	/*
 	std::cout << "cameraPos == vec3(" << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << ") " << '\n';
 
 	///Оказывается важна цель для LookAt Ведь это поставщики данныых
@@ -249,8 +352,7 @@ int main()
 	std::cout <<"cameraDirection == vec3("<< cameraDirection.x << ", " << cameraDirection.y << ", " << cameraDirection.z << ") " << '\n';
 
 	///Оказывается важна цель для LookAt  Ведь это поставщики данныых
-	glm::vec3 upWorld = glm::vec3(0.0f, 1.0f, 0.0f);
-
+	
 	glm::vec3 cameraRight = glm::normalize(glm::cross(upWorld, cameraDirection));
 
 	std::cout << "cameraRight == vec3(" << cameraRight.x << ", " << cameraRight.y << ", " << cameraRight.z << ") " << '\n';
@@ -261,31 +363,37 @@ int main()
 	std::cout << "cameraUp == vec3(" << cameraUp.x << ", " << cameraUp.y << ", " << cameraUp.z << ") " << '\n';
 
 	
-	glm::mat4 viewM = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.3f),
+	
+
+	glm::mat4 viewM = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
 									glm::vec3(0.0f, 0.0f, 0.0f),
 									glm::vec3(0.0f, 1.0f, 0.0f));
+	*/
+	//камера фронт это шаг вперед по оси Z
+	///Так же это гарантирует что камера всегда смотрит в направлении цели
+//	glm::mat4 viewM2 = glm::lookAt(cameraPos,	cameraPos + cameraFront ,upWorld);
 
 	/////////////////////////////////
 
-	glm::mat4 view(1.0f);
 
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	GLfloat radius = 15.0f;
+
+
+	///glm::mat4 view(1.0f);
+	///view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 
 	 ////////////////////
-	 glm::mat4  projection = glm::perspective(glm::radians(45.0f), (GLfloat)width / height, 0.1f, 100.0f);
+	
 
 	//Вызвай до использования присвоений в программу
 	ourShader.use();
 		
 	GLint modelLoc = glGetUniformLocation(ourShader.ID, "model");
-
-
 	GLint viewLoc = glGetUniformLocation(ourShader.ID, "view");
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
 	GLint projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	
 
 	////////////////////////////////////
 
@@ -310,14 +418,26 @@ int main()
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
+		glfwPollEvents();
+		do_movement();
 		///И вот ответ матрицу надо всегда сбрасывать до начальных значений на каждой итерации
 		///иначе значения вращения будут прибавляться по нарастающей
 		///Так как матрица объявлена здесь то и отправка ее в шейдер пойдет после объявления
 		/// Ну и преписывать матрицу в область памяти ГПУ нужно после изменения :))
-			
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 		
+		//GLfloat camX = sin(glfwGetTime()) * radius;
+		//GLfloat camZ = cos(glfwGetTime()) * radius;
 
+
+		auto viewM2 = glm::lookAt(cameraPos, cameraPos+ cameraFront, upWorld);
+		//auto view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(viewM2));
+
+		glm::mat4  projection = glm::perspective(glm::radians(fov), (GLfloat)width / height, 0.1f, 100.0f);
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
 
@@ -335,7 +455,7 @@ int main()
 
 		glfwSwapBuffers(window);
 		/// Проверяем события и вызываем функции обратного вызова.
-		glfwPollEvents();
+		
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
